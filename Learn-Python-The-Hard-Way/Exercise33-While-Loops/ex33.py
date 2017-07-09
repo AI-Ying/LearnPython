3d252525f4af3017b08dd444e8776f84a4326ae3
AI-Ying <977551573@qq.com>
2017-07-10 00:17:21 +0800

    Learn-Python-The-Hard-Way

--- Learn-Python-The-Hard-Way/Exercise33-While-Loops/ex33.py
+++ Learn-Python-The-Hard-Way/Exercise33-While-Loops/ex33.py
@@ -1,17 +1,24 @@
 # -*- coding:utf-8 -*-
 
-i = 0
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def while_loop(i, p):
+    while i < p:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+    return numbers
 
-print "The number: "
+
+# call while_loop:
+print "we can call the function of while_loop"
+while_loop(3, 8)
+
+
+print "The numbers now:"
 
 for num in numbers:
     print num
