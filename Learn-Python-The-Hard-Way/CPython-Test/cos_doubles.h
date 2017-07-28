7be2616dc54188febff17a003e9f8325b0bfc3dd
AI-Ying <977551573@qq.com>
2017-07-28 20:48:29 +0800

    Learn-Python-The-Hard-Way

---
+++ Learn-Python-The-Hard-Way/CPython-Test/cos_doubles.h
@@ -0,0 +1,6 @@
+# ifndef _COS_DOUBLES_H
+# define _COS_DOUBLES_H
+
+void vos_doubles(double * in_array, double * out_array, int size);
+
+# endif
---
+++ Learn-Python-The-Hard-Way/CPython-Test/test.py
@@ -0,0 +1,11 @@
+import cos_doubles
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.linspace(-5, 5, 100)
+b = np.empty_like(a)
+cos_doubles.cos_doubles_func(a, b)
+
+plt.plot(b)
+plt.show()
+
