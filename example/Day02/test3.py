d377c7ad75c1b80f7a1cb9422d061646f45431b0
AI-Ying <977551573@qq.com>
2019-02-06 16:28:55 +0800

    :

--- example/Day02/test3.py
+++ example/Day02/test3.py
@@ -3,7 +3,7 @@
 # Author : Arthur Yan
 # Date   : 2019-02-06 15:54:00
 
-year = int(input("Input year"))
+year = int(input("Input year:"))
 is_leap = (year % 4 == 0 and year % 100 == 0 or
     year % 400 == 0)
 print(is_leap)
