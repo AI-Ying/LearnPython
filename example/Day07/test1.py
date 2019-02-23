6ae91fe3d90a90d98eb9702c57767d965decc0f5
AI-Ying <977551573@qq.com>
2019-02-23 10:34:10 +0800

    Day0

---
+++ example/Day07/test1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+# -*- coding:utf8 -*-
+# Author      : Arthur Yan
+# Date        : 2019-02-17 16:36:03
+# Description : 字符串使用
+
+
+def main():
+    """docstring for main"""
+    str1 = "hello, world!"
+    print(len(str1))
+    print(str1.capitalize())
+    print(str1.upper())
+
+    print(str1.find('or'))
+    print(str1.find('ai'))
+    # 若未查到，则报异常
+    # print(str1.index('or'))
+    # print(str1.index('ai'))
+
+    print(str1.startswith("He"))  # 检查字符串以什么开始
+    print(str1.startswith("hel"))
+    print(str1.endswith('!'))  # 检查字符串以什么结尾
+
+    # 将字符串以指定宽度居中，并在两侧填充指定的字符
+    print(str1.center(50, '*'))
+    # 将字符串以指定宽度放右，并在左侧填充指定字符串
+    print(str1.rjust(50, ' '))
+
+    str2 = 'abc123456'
+    # sequence[starting_index:ending_index:step]
+    # ------------------------------------------
+    # -----|-a-|-b-|-c-|-1-|-2-|-3-|-4-|-5-|-6-|
+    # -下标| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
+    # -下标|-9 |-8 |-7 |-6 |-5 |-4 |-3 |-2 |-1 |
+    # ------------------------------------------
+    print(str2[2])
+    print(str2[2:5])
+    print(str2[2:])
+    print(str2[2::2])
+    print(str2[::2])
+    print(str2[::-1])
+    print(str2[-3:-1])
+
+    # 检查字符串是否由数字构成
+    print(str2.isdigit())
+    # 检查字符串是否由字符串构成
+    print(str2.isalpha())
+    # 检查字符串是否由字符串和数字构成
+    print(str2.isalnum())
+
+    str3 = '  ai@ying.com '
+    print(str3)
+    # 获得字符串修剪两侧空格的拷贝
+    print(str3.strip())
+
+
+if __name__ == "__main__":
+    main()
---
+++ example/Day07/test2.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+# -*- coding:utf8 -*-
+# Author      : Arthur Yan
+# Date        : 2019-02-17 19:07:49
+# Description : 列表的使用
+
+import sys
+
+
+def main():
+    """docstring for main"""
+    list1 = [1, 3, 5, 7, 100]
+    print(list1)
+    list2 = ['hello']*5
+    print(list2)
+    print(len(list2))
+    print(list1[0])
+    print(list1[4])
+    print(list1[-1])
+    print(list1[-3])
+    list1[2] = 200
+    print(list1)
+    list1.append(300)
+    list1.insert(2, 400)
+    list1 += [1000, 2000]
+    print(list1)
+    list1.remove(7)
+    print(list1)
+    if 3 in list1:
+        list1.remove(3)
+    print(list1)
+    del list1[0]
+    print(list1)
+
+    list1.clear()
+    print(list1)
+
+    # 切片
+    fruits = ['grape', 'apple', 'strawberry', 'waxberry']
+    fruits += ['pitaya', 'pear', 'mango']
+
+    # 循环遍历列表元素
+    for fruit in fruits:
+        print(fruit.title(), end=' ')
+    print()
+
+    fruits2 = fruits[:]
+    print(fruits2)
+    fruits3 = fruits2[::-1]
+    print(fruits3)
+
+    fruits4 = sorted(fruits)
+    fruits5 = sorted(fruits, reverse=True)
+    print(fruits)
+    print(fruits4)
+    print(fruits5)
+
+    fruits6 = sorted(fruits, key=len)
+    print(fruits6)
+    fruits.sort(reverse=True)
+    print(fruits)
+
+    # 创建列表
+    f = [x for x in range(1, 10)]
+    print(f)
+    f = [x + y for x in 'ABCDEF' for y in '1234567']
+    print(f)
+    f = [x ** 2 for x in range(1, 10)]
+    print(f)
+    print(sys.getsizeof(f))
+
+    # 使用生成器,不占用数据存储空间
+    # 也可以使用yield关键字改造成生成器函数
+    f = (x ** 2 for x in range(1, 10))
+    print(f)
+    print(sys.getsizeof(f))
+
+    for var in f:
+        print(var, end=' ')
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
